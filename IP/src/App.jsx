a4296128fc4145113e578115802128af0a01bfeb
parvez48 <khaledparvez45@gmail.com>
2024-10-28 11:14:03 +0600

    Dynamic basic

---
+++ IP/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemList from "./Component/ItemList";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Dynamic Content Project!</h1>
+      <ItemList />
+    </div>
+  );
+}
+
+export default App;
