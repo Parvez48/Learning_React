443d3a38311c4116b05a5c1df6b5000d97e2de8a
parvez48 <khaledparvez45@gmail.com>
2024-10-28 12:44:28 +0600

    NameList

--- IP/src/App.jsx
+++ IP/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import ToggleContent from "./Component/ToggleCounter";
+import NameList from "./Component/NameList";
 
 function App() {
   return (
     <div>
       <h1>Welcome to My Dynamic Content Project!</h1>
-      <ToggleContent />
+      <NameList />
     </div>
   );
 }
